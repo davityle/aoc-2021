11408c61fa516d42dd5f24b575b2e028b5f3ccce
tyler <td@tyler-davis.com>
2021-12-29 22:48:42 -0700

    day 17 part 1

---
+++ day17/src/main.rs
@@ -0,0 +1,60 @@
+fn main() {
+    let vector = find_max_height();
+    println!("{:?}", vector);
+    println!("{:?}", get_max_height(vector));
+}
+
+fn find_max_height() -> (i32, i32) {
+    let mut max_height = 0;
+    let mut max_height_vector = (0, 0);
+    let max_single_magnitude =
+        (INPUT[1].0.abs() - INPUT[0].0.abs()) * (INPUT[1].1.abs() - INPUT[0].1.abs());
+
+    for x in 0..max_single_magnitude {
+        for y in 0..max_single_magnitude {
+            let dir_vector = (x, y);
+
+            match get_max_height(dir_vector) {
+                Some(height) => {
+                    if height > max_height {
+                        max_height = height;
+                        max_height_vector = dir_vector;
+                    }
+                }
+                None => {}
+            }
+        }
+    }
+
+    max_height_vector
+}
+
+fn get_max_height(mut dir_vector: (i32, i32)) -> Option<i32> {
+    let mut position = (0, 0);
+    let mut max_height = 0;
+
+    while !is_in_box(position, INPUT) && position.0 < INPUT[1].0 && position.1 > INPUT[1].1 {
+        position.0 += dir_vector.0;
+        position.1 += dir_vector.1;
+
+        if dir_vector.0 > 0 {
+            dir_vector.0 -= 1;
+        }
+        dir_vector.1 -= 1;
+        if position.1 > max_height {
+            max_height = position.1;
+        }
+    }
+    if !is_in_box(position, INPUT) {
+        None
+    } else {
+        Some(max_height)
+    }
+}
+
+fn is_in_box(point: (i32, i32), b: [(i32, i32); 2]) -> bool {
+    b[0].0 <= point.0 && point.0 <= b[1].0 && b[0].1 >= point.1 && point.1 >= b[1].1
+}
+
+const _: [(i32, i32); 2] = [(20, -5), (30, -10)];
+const INPUT: [(i32, i32); 2] = [(56, -134), (76, -162)];
